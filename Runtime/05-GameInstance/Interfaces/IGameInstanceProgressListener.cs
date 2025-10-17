def2ff542e8149435eeff0fca6d3df62d45a9e93
Brad Nguyen <brad.nguyen@jk.technology>
2025-10-17 09:14:28 +0700

    Update IGameInstanceProgressListener.cs

--- Runtime/05-GameInstance/Interfaces/IGameInstanceProgressListener.cs
+++ Runtime/05-GameInstance/Interfaces/IGameInstanceProgressListener.cs
@@ -3,6 +3,6 @@ namespace JKTechnologies.SeensioGo.GameInstances
     public interface IGameInstanceProgressListener
     {
         public void SetupTargetScore(int targetScore, int currentPlayerScore);
-        public void OnPlayerStoreUpdated(int currentPlayerScore);
+        public void OnPlayerScoreUpdated(int currentPlayerScore);
     }
 }
